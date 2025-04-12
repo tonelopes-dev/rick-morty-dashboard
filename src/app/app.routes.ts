4b9ef09d1e1f24ff774596358c688ed905bc6d5f
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-12 14:29:12 -0300

    feat: add routes in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./pages/characters-list/characters-list.component').then(
+        (m) => m.CharactersListComponent
+      ),
+  },
+  {
+    path: 'episodes',
+    loadComponent: () =>
+      import('./pages/episodes-list/episodes-list.component').then(
+        (m) => m.EpisodesListComponent
+      ),
+  },
+  {
+    path: 'detail/:id',
+    loadComponent: () =>
+      import('./pages/detail/detail.component').then((m) => m.DetailComponent),
+  },
+  { path: '', redirectTo: '/characters', pathMatch: 'full' },
+];
