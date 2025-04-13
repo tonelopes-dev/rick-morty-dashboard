a309a04659046e9455084e25645634e0180b020b
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-13 16:42:37 -0300

    chore: create environment.ts file to store API base URL

--- src/app/services/rick-morty.service.ts
+++ src/app/services/rick-morty.service.ts
@@ -4,57 +4,41 @@ import { ApiResponse } from '../types/common';
 import { Character } from '../types/character';
 import { Episode } from '../types/episode';
 import { expand, EMPTY, reduce } from 'rxjs';
+import { environment } from 'environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RickMortyService {
-  private apiUrl = 'https://rickandmortyapi.com/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   // Métodos para Characters
-  getAllCharacters() {
-    return this.http
-      .get<ApiResponse<Character>>(`${this.apiUrl}/character`)
-      .pipe(
-        expand((res) =>
-          res.info.next
-            ? this.http.get<ApiResponse<Character>>(res.info.next)
-            : EMPTY
-        ),
-        reduce((acc, res) => acc.concat(res.results), [] as Character[])
-      );
-  }
-
-  getCharacter(id: number) {
-    return this.http.get<Character>(`${this.apiUrl}/character/${id}`);
-  }
-
-  // rick-morty.service.ts
-  getMultipleCharacters(ids: number[]) {
-    return this.http.get<Character[]>(
-      `${this.apiUrl}/character/${ids.join(',')}`
+  getCharacters(page: number) {
+    return this.http.get<ApiResponse<Character>>(
+      `${this.apiUrl}/character?page=${page}`
     );
   }
 
+  getCharactersByName(name: string) {
+    return this.http.get<ApiResponse<Character>>(
+      `${this.apiUrl}/character?name=${name}`
+    );
+  }
+
+  getCharacter(id: number) {
+    return this.http.get<Character>(`${this.apiUrl}/character/${id}`) ?? {};
+  }
+
   // Métodos para Episodes
-  getEpisodes(page: number = 1) {
+  getEpisodes(page: number) {
     return this.http.get<ApiResponse<Episode>>(
       `${this.apiUrl}/episode?page=${page}`
     );
   }
 
-  getAllEpisodes() {
-    return this.http.get<ApiResponse<Episode>>(`${this.apiUrl}/episode`).pipe(
-      expand((res) =>
-        res.info.next
-          ? this.http.get<ApiResponse<Episode>>(res.info.next)
-          : EMPTY
-      ),
-      reduce((acc, res) => acc.concat(res.results), [] as Episode[])
-    );
-  }
+
 
   getEpisode(id: number) {
     return this.http.get<Episode>(`${this.apiUrl}/episode/${id}`);
