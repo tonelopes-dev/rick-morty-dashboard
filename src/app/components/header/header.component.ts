4513bd0007ad41f99918dcfa6afa55f3c1b600b1
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-13 16:45:30 -0300

    feat: add onSearch method and isListPage utility to header for dynamic input behavior and styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,24 +3,28 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SearchService } from '../../services/search.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   private router = inject(Router);
+  private searchService = inject(SearchService);
   searchTerm = '';
 
   isListPage(): boolean {
-    return this.router.url.includes('/characters') ||
-           this.router.url.includes('/episodes');
+    return (
+      this.router.url.includes('/characters') ||
+      this.router.url.includes('/episodes')
+    );
   }
 
-  onSearch() {
-    // Implementaremos a lógica de busca posteriormente
+  onSearch(inputValue: string) {
+    this.searchService.setSearchTerm(inputValue);
   }
 }
