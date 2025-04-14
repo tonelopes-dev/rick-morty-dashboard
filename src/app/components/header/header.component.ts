b5dac11b5a82db7547bf1c9a53cfa7ce7178f449
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-14 09:36:30 -0300

    feat: add ngOnInit and ngOnDestroy lifecycle hooks to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 // header.component.ts
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SearchService } from '../../services/search.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +14,26 @@ import { SearchService } from '../../services/search.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   private router = inject(Router);
   private searchService = inject(SearchService);
+  private _unsubscribe$ = new Subject<void>();
+
   searchTerm = '';
+
+  ngOnInit() {
+    this.searchService
+      .getSearchTerm()
+      .pipe(takeUntil(this._unsubscribe$))
+      .subscribe((term) => {
+        this.searchTerm = term;
+      });
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
+  }
 
   isListPage(): boolean {
     return (
@@ -24,6 +42,7 @@ export class HeaderComponent {
     );
   }
 
+  // se for vazio chama a primeira página
   onSearch(inputValue: string) {
     this.searchService.setSearchTerm(inputValue);
   }
