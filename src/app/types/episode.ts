2a6d982b7725a039df00d7c15dab3c8d49be87bd
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-13 22:36:02 -0300

    feat: create EpisodeApiResponse interface for episode data structure

--- src/app/types/episode.ts
+++ src/app/types/episode.ts
@@ -1,3 +1,4 @@
+
 export interface Episode {
   id: number;
   name: string;
@@ -6,4 +7,13 @@ export interface Episode {
   characters: string[];
   url: string;
   created: string;
+}
+export interface EpisodeApiResponse {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  results: Episode[];
 }
