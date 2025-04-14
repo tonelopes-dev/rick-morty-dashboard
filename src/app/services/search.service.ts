03baf765d19a442b564800a9a4530c66e46d164c
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-13 22:37:13 -0300

    feat: create SearchService to manage search term state with RxJS

---
+++ src/app/services/search.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  BehaviorSubject,
+} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SearchService {
+  private searchTerm$ = new BehaviorSubject<string>('');
+  searchState$ = this.searchTerm$.asObservable();
+
+  setSearchTerm(term: string) {
+    this.searchTerm$.next(term);
+  }
+
+  getSearchTerm() {
+    return this.searchTerm$.asObservable();
+  }
+}
