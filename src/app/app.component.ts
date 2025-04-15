6cbed46ce1486754aea7b3784f3f4e29fd4edab0
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-14 22:28:27 -0300

    feat: add login route, apply CSS styles, and update layout in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'rick-morty-dashboard';
+
+  constructor(private router: Router) {}
+
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
+  }
 }
