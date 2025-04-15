c4e363c1deefdd1b50401cca2a8e177d5e52cfdb
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-15 14:28:25 -0300

    chore: implement HTTP interceptor in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthGuard } from './guards/auth.guard';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     AuthGuard
   ],
 };
