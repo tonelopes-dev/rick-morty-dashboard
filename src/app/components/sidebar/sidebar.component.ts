764736dec7fb2ecdfe322e9c20f34ea35ff184f8
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-14 22:31:58 -0300

    feat: update sidebar component with ngOnInit and logout functionality

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,32 @@
 // sidebar.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '@app/services/auth.service';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  username: string = '';
+  avatar: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    const authData = this.authService.getAuthData();
+    if (authData) {
+      this.username = authData.username;
+      this.avatar = authData.avatar;
+    }
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
